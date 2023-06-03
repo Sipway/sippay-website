1990acbdcc09e99dc2682b36c32d2d864d46fd79
Phani-Gorantla <phanigorantla531@gmail.com>
2023-06-03 22:51:00 +0530

    1st version

--- src/components/maintitle/MainTitle.js
+++ src/components/maintitle/MainTitle.js
@@ -2,18 +2,20 @@ import React from 'react';
 import './MainTitle.css';
 
 function MainTitle() {
+  const handleJoinWaitlist = () => {
+    window.open('https://forms.gle/BLiwJAYFxw9r4EjW8', '_blank');
+  };
+
   return (
     <div className="main-title">
       <div className="left-column">
-        <h2 className="main-heading">
-          Earn Instant rewards when you shop
-        </h2>
-        <p className="sub-heading">
-          Collect rewards at all your favourite stores in India
-        </p>
+        <h2 className="main-heading">Earn Instant rewards when you shop</h2>
+        <p className="sub-heading">Collect rewards at all your favourite stores in India</p>
       </div>
       <div className="right-column">
-        <button className="join-button">Join the Waitlist</button>
+        <button className="join-button" onClick={handleJoinWaitlist}>
+          Join the Waitlist
+        </button>
       </div>
     </div>
   );
