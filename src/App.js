7db7ca0c96067a9a0cee2682c7fef1b8e0a3814b
Phani-Gorantla <phanigorantla531@gmail.com>
2023-06-03 16:48:34 +0530

    1st version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import './App.css';
 import Brands from './components/Brands/Brands';
+import InfoBox1 from './components/InfoBox1/InfoBox1';
+import InfoBox2 from './components/InfoBox2/InfoBox2';
 import ReverseBrand from './components/ReverseBrand/ReverseBrand';
 import Header from './components/header/Header';
 import MainTitle from './components/maintitle/MainTitle';
+import Why from './components/why/Why';
 
 function App() {
  
@@ -16,6 +19,11 @@ function App() {
       <Brands />
       <div style={{ margin: '20px' }}></div> {/* Add a margin for the gap */}
       <ReverseBrand />
+      <div style={{ margin: '80px' }}></div> {/* Add a margin for the gap */}
+      <Why />
+      <div style={{ margin: '80px' }}></div> {/* Add a margin for the gap */}
+      <InfoBox1 />
+      <InfoBox2 />
     </div>
   );
 }
---
+++ src/components/why/Why.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Why.css';
+
+const Why = () => {
+  return (
+    <div className="why-container">
+      <span className="why-text">Why SIPPAY is Better than Credit Cards ?</span>
+      <i className="why-icon fas fa-chevron-down"></i>
+    </div>
+  );
+}
+
+export default Why;
