c8a690eaded0ab6a7628a0734146a4907e2dcd2d
Phani-Gorantla-au45 <38903564+Phani-Gorantla-au45@users.noreply.github.com>
2023-06-06 09:28:40 +0530

    Revert "Look And Feel Enhancement"

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-scroll';
 
 function Header() {
   return (
-    <header className="wrapper">
-      <div className="wrapper-item-logo">
-        <span className="logo gold-text">SIP</span>
-        <span className="logo white-text">PAY</span>
-      </div>
-      <div className="wrapper-item-know-more-button">
-        <Link
-          className="know-more-button"
-          to="info-grid-section"
-          smooth={true}
-          duration={500}
-          offset={-50}
-        >
-          Know More
-        </Link>
+    <header className="header">
+      <div className="logo">
+        <span className="gold-text">SIP</span>
+        <span className="white-text">PAY</span>
       </div>
     </header>
   );
--- src/components/maintitle/MainTitle.js
+++ src/components/maintitle/MainTitle.js
@@ -5,16 +5,17 @@ function MainTitle() {
   const handleJoinWaitlist = () => {
     window.open('https://forms.gle/BLiwJAYFxw9r4EjW8', '_blank');
   };
+
   return (
     <div className="main-title">
-      <div>
-        <div className="main-heading">Earn Instant rewards when you shop</div>
-        <div className="sub-heading">across all your favourite stores in India</div>
-        <div className="wrapper-item-waitlist-button">
-          <button className="join-button" onClick={handleJoinWaitlist}>
-            Join the Waitlist
-          </button>
-        </div>
+      <div className="left-column">
+        <h2 className="main-heading">Earn Instant rewards when you shop</h2>
+        <p className="sub-heading">Collect rewards at all your favourite stores in India</p>
+      </div>
+      <div className="right-column">
+        <button className="join-button" onClick={handleJoinWaitlist}>
+          Join the Waitlist
+        </button>
       </div>
     </div>
   );
